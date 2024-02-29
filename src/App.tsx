dc8ce4aed8b9d01d1964eedab3e137d6ba9832ac
danielmachado <danielmachadopintos@gmail.com>
2024-02-29 15:54:44 -0300

    Atualizar estilos e componentes da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
-import "./App.css"; // Importando o arquivo CSS
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import TextField from "@mui/material/TextField";
+import "./App.css";
+import styled from "styled-components";
+
+const HeaderTitle = styled(Typography)`
+  flex-grow: 1;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
 
 function App() {
   return (
     <div className="background-image">
-      <div className="search-bar">
-        <input type="text" placeholder="Pesquisar..." />
+      <AppBar style={{ height: "80px" }} color="primary">
+        <Toolbar>
+          <HeaderTitle variant="h6">
+            <img
+              className="img-logo"
+              src="/logo192.png"
+              alt="Logo"
+              style={{ height: "25px", marginRight: "10px" }}
+            />
+            Find your Personal
+          </HeaderTitle>
+          <IconButton color="inherit">
+            <AccountCircle />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <div className="content">
+        <Typography variant="h1" component="h1" gutterBottom>
+          Personal Trainer
+        </Typography>
+        <div className="search-bar">
+          <TextField fullWidth label="Pesquisar..." variant="outlined" />
+        </div>
       </div>
     </div>
   );
